be8c21d9a89134f2e3619fe5edeb161c18d7ffc4
Maksim <maksim.vladimirov@neuro-city.ru>
2023-05-30 12:00:30 +0300

    add new solution

--- Arrays/1.TwoSum.js
+++ Arrays/1.TwoSum.js
@@ -20,6 +20,8 @@
 // 	const element = array[index];
 // }
 
+
+// Скорость алгоритма  O(n2)
 var twoSum = function (nums, target) {
 	for (let i = 0; i < nums.length; i++) {
 		for (let j = i + 1; j < nums.length; j++) {
@@ -29,5 +31,20 @@ var twoSum = function (nums, target) {
 		}
 	}
 };
+
+// Скорость алгоритма O(n)
+var twoSum = function(nums, target) {
+	let map = new Map();
+  
+	for (let i = 0; i < nums.length; i ++) {
+	  const diff = target - nums[i];
+	  
+	  if (map.has(diff)) {
+		return [i, map.get(diff)]
+	  }
+  
+	  map.set(nums[i], i)
+	}
+  };
 
 console.log(twoSum([3,2,3], 6))
